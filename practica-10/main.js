8fdd552515583f5bab0ac5614906cbeaf3f147f9
Wichs03 <wicheyts@gmail.com>
2025-06-20 05:57:26 -0600

    practica 10 con api dando respuesta positiva

--- practica-10/main.js
+++ practica-10/main.js
@@ -2,7 +2,7 @@ const contenedor = document.querySelector(".contenedor");
 
 async function renderizarCards() {
   try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/user");
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
 
     contenedor.innerHTML = "";
